841db77ad0eb10694e185b70305ef447e76b233a
Travis Whitworth <traviswhitworth@BCs-MacBook-Pro-5.local>
2020-10-13 10:00:17 -0500

    completed JS Basics Exercise

---
+++ jsBasicsexercise.js
@@ -0,0 +1,36 @@
+console.log(20);
+
+// Numbers, Strings, Booleans, Null, and Undefined
+
+// + - * / %
+
+console.log(7%4);
+
+console.log('I\'M MARY POPPINS Y\'ALL!');
+
+
+console.log("Spider" + "-" + "man");
+
+console.log("Travis".length + "Whitworth".length);
+
+console.log("Avengers: Endgame"[16]);
+
+console.log("\\");
+
+console.log(5**4-9);
+
+console.log("I".length - "AM".length + "IRON".length - "MAN".length);
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+var m = 10;
+var c = 5;
+var u = m + c;
+console.log(u);
